e441c9628227b5513aa69dbefa6339d820e2edca
Zheng Tan <61394893+ZT220501@users.noreply.github.com>
2023-05-21 15:10:12 -0700

    Animation addded

--- Finite-Difference/Unidirectional_Flow_Explicit.py
+++ Finite-Difference/Unidirectional_Flow_Explicit.py
@@ -12,13 +12,12 @@ This code is a modification of the code in C. Pozrikidis's book
 '''
 
 
-'''
-TODO: Replace pressure gradient to be non-constant
-'''
+
 
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 '''
@@ -26,22 +25,35 @@ Initialze Values
 '''
 h = 1
 mu = 0.6 
-rho = 5.5
+#rho = 5.5
+rho = 20
 N = 32
 dpdx = -2
+#gx=0, represents that the channel is completely horizontal
 gx = 0
 V1 = 0
 V2 = 0
 
 #The diffusion number
 #The method is stable iff alpha<=0.5
-al = 0.49                       #Alpha value                  
-nstep = 20000                   #Step numbers
+al = 0.49                           #Alpha value                  
+nstep = 2000                        #Step numbers
 
 #Prepare
-nu = mu/rho                     #kinematic viscosity
+nu = mu/rho                         #kinematic viscosity
 Dy = h/N
 Dt = al*Dy*Dy/nu
+
+
+'''
+Velocity for upper and lower bound
+'''
+oscillation_const = 1
+def V1(t):
+    return np.sin(oscillation_const*t)
+
+def V2(t):
+    return np.cos(oscillation_const*t)
 
 
 
@@ -54,24 +66,67 @@ for i in range(N+1):
     y.append(i*Dy)
     u.append(0)
     
-u[0] = V1
-u[N] = V2
+u[0] = V1(0)
+u[N] = V2(0)
 
 '''
 Run Iterations
 '''
+u_total = [u]
 t = 0
 for step in range(nstep):
     t += Dt
-    unew = [V1]
+    u = u_total[-1]
+    unew = [V1(t)]
     for i in range(1, N):
         unew.append(al*u[i-1] + (1-2*al)*u[i] + al*u[i+1] + Dt*(-dpdx/rho+gx))
-    unew.append(V2)
-    u = unew
-    plt.plot(u, y, 'o-')
-    plt.axis([0, 0.5, 0, h])
-    plt.pause(0.01)
-        
+    unew.append(V2(t))
+    u_total.append(unew)
+
+
+
+'''
+Plot the result animation
+'''
+# creating a blank window
+# for the animation
+fig = plt.figure()
+axis = plt.axes(xlim =(-5, 5),
+                ylim =(0, h))
+axis.set_xlabel('u')
+axis.set_ylabel('y')
+ 
+line, = axis.plot([], [], lw = 2)
+ 
+# what will our line dataset
+# contain?
+def init():
+    line.set_data([], [])
+    return line,
+ 
+# initializing empty values
+# for x and y co-ordinates
+xdata, ydata = [], []
+ 
+# animation function
+def animate(i):
+
+    line.set_data(u_total[i], y)
+    line.set_marker('o')
+     
+    return line,
+ 
+# calling the animation function    
+anim = animation.FuncAnimation(fig, animate,
+                            init_func = init,
+                            frames = nstep,
+                            interval = 20,
+                            blit = True)
+ 
+# saves the animation in our desktop
+anim.save('uniflow_explicit_oscillation1_low_viscosity.gif', writer = 'ffmpeg', fps = 30)
+
+
 
 
 
