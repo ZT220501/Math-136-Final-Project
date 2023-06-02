11ab9ea26b220a71bd815543f31a9fef4cde6371
Zheng Tan <61394893+ZT220501@users.noreply.github.com>
2023-06-02 11:11:34 -0700

    Added exact solution validation

--- Finite-Difference/Unidirectional_Flow_Explicit.py
+++ Finite-Difference/Unidirectional_Flow_Explicit.py
@@ -18,6 +18,8 @@ This code is a modification of the code in C. Pozrikidis's book
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import time
+import copy
 
 
 '''
@@ -25,8 +27,8 @@ Initialze Values
 '''
 h = 1
 mu = 0.6 
-#rho = 5.5
-rho = 20
+rho = 0.5
+#rho = 20
 N = 32
 dpdx = -2
 #gx=0, represents that the channel is completely horizontal
@@ -48,12 +50,14 @@ Dt = al*Dy*Dy/nu
 '''
 Velocity for upper and lower bound
 '''
-oscillation_const = 1
+oscillation_const = 100
 def V1(t):
-    return np.sin(oscillation_const*t)
+    return 0
+    #return np.sin(oscillation_const*t)
 
 def V2(t):
-    return np.cos(oscillation_const*t)
+    return 0
+    #return np.cos(oscillation_const*t)
 
 
 
@@ -74,6 +78,8 @@ Run Iterations
 '''
 u_total = [u]
 t = 0
+#Track for running time
+start = time.time()
 for step in range(nstep):
     t += Dt
     u = u_total[-1]
@@ -81,7 +87,10 @@ for step in range(nstep):
     for i in range(1, N):
         unew.append(al*u[i-1] + (1-2*al)*u[i] + al*u[i+1] + Dt*(-dpdx/rho+gx))
     unew.append(V2(t))
-    u_total.append(unew)
+    u_total.append(copy.deepcopy(unew))
+
+print("Explicit finite difference running time: " + str(time.time()-start) + " seconds.")
+
 
 
 
@@ -97,16 +106,7 @@ axis.set_xlabel('u')
 axis.set_ylabel('y')
  
 line, = axis.plot([], [], lw = 2)
- 
-# what will our line dataset
-# contain?
-def init():
-    line.set_data([], [])
-    return line,
- 
-# initializing empty values
-# for x and y co-ordinates
-xdata, ydata = [], []
+
  
 # animation function
 def animate(i):
@@ -118,13 +118,90 @@ def animate(i):
  
 # calling the animation function    
 anim = animation.FuncAnimation(fig, animate,
-                            init_func = init,
                             frames = nstep,
                             interval = 20,
-                            blit = True)
+                            repeat = False)
  
 # saves the animation in our desktop
-anim.save('uniflow_explicit_oscillation1_low_viscosity.gif', writer = 'ffmpeg', fps = 30)
+anim.save('test.gif', writer = 'ffmpeg', fps = 30)
+
+
+
+print("I've finished computing.")
+
+'''
+Test by comparing to the analytic solution
+'''
+#Analytic solution to the constant case
+def u_analytic(t,y):
+    C = -dpdx/rho + gx
+    r = 0
+    for n in range(1,500):
+      if n % 2 == 1:
+        p1 = 4*C/(n**3*(np.pi)**3*nu) - 4*C/(n**3*(np.pi)**3*nu)*np.exp(-n**2*(np.pi**2)*nu*t)
+        r += p1 * np.sin(n*np.pi*y)
+    return r 
+# =============================================================================
+# def u_analytic(t,y):
+#     C = -dpdx/rho + gx
+#     k = 10
+#     gamma = (1-y) * np.sin(k*t) + y * np.cos(k*t)
+#     r = 0
+#     for n in range(1,500):
+#       den = n*np.pi*(n**4*(np.pi)**4*nu**2+k**2)
+#       if n % 2 == 1:
+#         s1 = 2*k*np.cos(k*t)*(n**2*(np.pi)**2*nu+k) / den
+#         s2 = 2*k*np.sin(k*t)*((n**2)*(np.pi)**2*nu-k) / den
+#         s3 = 4*C/(n**3*(np.pi)**3*nu)
+#         p1 = -s1 + s2 + s3
+#         p2 = -2/(n*np.pi) + 2*k*(n**2*(np.pi)**2*nu+k) / den - s3
+#         p3 = p2 * np.exp(-n**2*(np.pi)**2*nu*t)
+#         u = (p1 + p3) * np.sin(n*np.pi*y)
+#         r += u
+#       else:
+#         s1 = 2*k*np.cos(k*t)*(n**2*(np.pi)**2*nu-k) / den
+#         s2 = 2*k*np.sin(k*t)*(n**2*(np.pi)**2*nu+k) / den
+#         p1 = -s1 - s2
+#         p2 = 2/(n*np.pi) + 2*k*(n**2*(np.pi)**2*nu-k) / den
+#         p3 = p2 * np.exp(-n**2*(np.pi)**2*nu*t)
+#         u = (p1 + p3) * np.sin(n*np.pi*y)
+#         r += u
+#     result = gamma + r
+#     return result
+# =============================================================================
+# =============================================================================
+# def u_analytic(t,y):
+#     C = -dpdx/rho + gx
+#     r = 0
+#     gamma = 0.5 + 0.5*y
+#     for n in range(1,500):
+#       if n % 2 == 1:
+#         p = 4*C/(n**3*(np.pi)**3*nu) - (3/(n*np.pi)+4*C/(n**3*(np.pi)**3*nu))*np.exp(-n**2*(np.pi)**2*nu*t)
+#         r += p * np.sin(n*np.pi*y)
+#       else:
+#         p = np.exp(-n**2*(np.pi)**2*nu*t)/(n*np.pi)
+#         r += p * np.sin(n*np.pi*y)
+#     return r + gamma
+# =============================================================================
+
+u_anal = []
+for i in range(nstep+1):
+    u_actual_store = []
+    for j in range(N+1):
+        u_actual_store.append(u_analytic(i*Dt, j*Dy))
+    u_anal.append(u_actual_store)
+
+
+err = []
+for i in range(len(u_total)):
+    u_total[i] = np.array(u_total[i])
+    u_anal[i] = np.array(u_anal[i])
+    diff = u_total[i] - u_anal[i]
+    d = np.sqrt(Dy * np.dot(diff, diff))
+    err.append(d)
+err = np.array(err)
+print("Maximum error:", np.amax(err))
+print("Minimum error:", np.amin(err))
 
 
 
--- Finite-Difference/Unidirectional_Flow_Implicit.py
+++ Finite-Difference/Unidirectional_Flow_Implicit.py
@@ -19,25 +19,39 @@ TODO: Change the tridiagonal solver to others such as the Arnoldi/Lanczos iterat
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import copy
+import time
 
 '''
 Initial Values
 '''
 h = 1.0
-mu = 0.2
+mu = 0.6
 rho = 0.5
 N = 32
 dpdx = -2.0
 gx = 0
-V1 = 0 
-V2 = 0
 
 #The diffusion number
-alpha = 0.4;               
-nstep = 20000;                          #Number of steps
-nu = mu/rho;
-Dy = h/N;
-Dt = alpha*Dy*Dy/nu;
+alpha = 0.4               
+nstep = 2000                            #Number of steps
+nu = mu/rho
+Dy = h/N
+Dt = alpha*Dy*Dy/nu
+
+
+'''
+Velocity for upper and lower bound
+'''
+oscillation_const = 10
+def V1(t):
+    #return 0.5
+    return np.sin(oscillation_const*t)
+
+def V2(t):
+    #return 1
+    return np.cos(oscillation_const*t)
 
 '''
 Grid and initial condition
@@ -48,8 +62,8 @@ for i in range(N+1):
     y.append(i*Dy)
     u.append(0)
     
-u[0] = V1;
-u[N] = V2;
+u[0] = V1(0)
+u[N] = V2(0)
 '''
 Formulate the tridiagonal projection matrix
 atr is the diagonal line of the coefficient matrix
@@ -63,6 +77,8 @@ for i in range(N-1):
     atr.append(1.0 + 2*alpha)
     btr.append(-alpha)
     ctr.append(-alpha)
+    
+    
     
 #Tridiagonal Solver
 '''
@@ -99,26 +115,146 @@ def thomas(n,a,b,c,s):
         
     return x
 
+
+
 '''
 Run iterations
 '''
+u_total = [u]
+
 t = 0
+start = time.time()
 for step in range(nstep):
+    u = u_total[-1]
     s = []
     for i in range(N-1):
         s.append(u[i+1] + Dt * (-dpdx/rho + gx))
     t = t + Dt
-    u[0] = V1
-    u[N] = V2
-    s[0] = s[0] + alpha * u[0]
-    s[N-2] = s[N-2] + alpha * u[N]
+    
+    u_new = copy.deepcopy(u)
+    u_new[0] = V1(t)
+    u_new[N] = V2(t)
+    s[0] = s[0] + alpha * u_new[0]
+    s[N-2] = s[N-2] + alpha * u_new[N]
     sol = thomas(N-1,atr,btr,ctr,s);
     for i in range(1, N):
-        u[i] = sol[i-1]
+        u_new[i] = sol[i-1]
         
-    plt.plot(u, y, 'o-')
-    plt.axis([0, 0.5, 0, h])
-    plt.pause(0.01)
+    u_total.append(copy.deepcopy(u_new))
+        
+print("Implicit finite difference running time: " + str(time.time()-start) + " seconds.")
+
+'''
+Plot the result animation
+'''
+# creating a blank window
+# for the animation
+fig = plt.figure()
+axis = plt.axes(xlim =(-5, 5),
+                ylim =(0, h))
+axis.set_xlabel('u')
+axis.set_ylabel('y')
+ 
+line, = axis.plot([], [], lw = 2)
+
+ 
+# animation function
+def animate(i):
+
+    line.set_data(u_total[i], y)
+    line.set_marker('o')
+     
+    return line,
+ 
+# calling the animation function    
+anim = animation.FuncAnimation(fig, animate,
+                            frames = nstep,
+                            interval = 20,
+                            repeat = False)
+ 
+# saves the animation in our desktop
+anim.save('test.gif', writer = 'ffmpeg', fps = 30)
+
+
+
+
+print("I've finished computing.")
+
+'''
+Test by comparing to the analytic solution
+'''
+#Analytic solution to the constant case
+# =============================================================================
+# def u_analytic(t,y):
+#     C = -dpdx/rho + gx
+#     r = 0
+#     for n in range(1,500):
+#       if n % 2 == 1:
+#         p1 = 4*C/(n**3*(np.pi)**3*nu) - 4*C/(n**3*(np.pi)**3*nu)*np.exp(-n**2*(np.pi**2)*nu*t)
+#         r += p1 * np.sin(n*np.pi*y)
+#     return r 
+# =============================================================================
+# =============================================================================
+# def u_analytic(t,y):
+#     C = -dpdx/rho + gx
+#     k = 10
+#     gamma = (1-y) * np.sin(k*t) + y * np.cos(k*t)
+#     r = 0
+#     for n in range(1,500):
+#       den = n*np.pi*(n**4*(np.pi)**4*nu**2+k**2)
+#       if n % 2 == 1:
+#         s1 = 2*k*np.cos(k*t)*(n**2*(np.pi)**2*nu+k) / den
+#         s2 = 2*k*np.sin(k*t)*((n**2)*(np.pi)**2*nu-k) / den
+#         s3 = 4*C/(n**3*(np.pi)**3*nu)
+#         p1 = -s1 + s2 + s3
+#         p2 = -2/(n*np.pi) + 2*k*(n**2*(np.pi)**2*nu+k) / den - s3
+#         p3 = p2 * np.exp(-n**2*(np.pi)**2*nu*t)
+#         u = (p1 + p3) * np.sin(n*np.pi*y)
+#         r += u
+#       else:
+#         s1 = 2*k*np.cos(k*t)*(n**2*(np.pi)**2*nu-k) / den
+#         s2 = 2*k*np.sin(k*t)*(n**2*(np.pi)**2*nu+k) / den
+#         p1 = -s1 - s2
+#         p2 = 2/(n*np.pi) + 2*k*(n**2*(np.pi)**2*nu-k) / den
+#         p3 = p2 * np.exp(-n**2*(np.pi)**2*nu*t)
+#         u = (p1 + p3) * np.sin(n*np.pi*y)
+#         r += u
+#     result = gamma + r
+#     return result
+# =============================================================================
+def u_analytic(t,y):
+    C = -dpdx/rho + gx
+    r = 0
+    gamma = 0.5 + 0.5*y
+    for n in range(1,500):
+      if n % 2 == 1:
+        p = 4*C/(n**3*(np.pi)**3*nu) - (3/(n*np.pi)+4*C/(n**3*(np.pi)**3*nu))*np.exp(-n**2*(np.pi)**2*nu*t)
+        r += p * np.sin(n*np.pi*y)
+      else:
+        p = np.exp(-n**2*(np.pi)**2*nu*t)/(n*np.pi)
+        r += p * np.sin(n*np.pi*y)
+    return r + gamma
+
+u_anal = []
+for i in range(nstep+1):
+    u_actual_store = []
+    for j in range(N+1):
+        u_actual_store.append(u_analytic(i*Dt, j*Dy))
+    u_anal.append(u_actual_store)
+
+
+err = []
+for i in range(len(u_total)):
+    u_total[i] = np.array(u_total[i])
+    u_anal[i] = np.array(u_anal[i])
+    diff = u_total[i] - u_anal[i]
+    d = np.sqrt(Dy * np.dot(diff, diff))
+    err.append(d)
+err = np.array(err)
+print("Maximum error:", np.amax(err))
+print("Minimum error:", np.amin(err))
+
+
     
 
 
